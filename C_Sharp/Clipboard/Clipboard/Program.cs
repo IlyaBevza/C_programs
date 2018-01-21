0d778b1fc1e91aa7cce967de33acc1ea54f3fcdf
Ilya Bevza <ilya_bevza@hotmail.com>
2018-01-21 21:51:59 +0300

    Обработчик запроса для вставки в текст запроса 1с

--- C_Sharp/Clipboard/Clipboard/Program.cs
+++ C_Sharp/Clipboard/Clipboard/Program.cs
@@ -1,22 +1,47 @@
 ﻿
 using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace mClipboard
 {
 	class Program
 	{
+        [STAThread]
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			string clip=Clipboard.GetText();
-			
-			Console.WriteLine("{0}",clip);
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+            if (!Clipboard.ContainsText())
+            {
+                Console.WriteLine("No text in clipboard.");
+                Console.ReadKey();
+            }
+            else
+            {
+                string clip = Clipboard.GetText();
+                List<char> symbs = new List<char>(clip);
+                int num = 0;
+                foreach (char ch in clip)
+                {
+                    if (ch == '\"')
+                    {
+                        symbs.Insert(num + 1, '\"');
+                        num++;
+                    }
+                    else
+                    {
+                        if (ch == '\n'&& num!=symbs.Count-1)
+                        {
+                            symbs.Insert(num + 1, '|');
+                            num++;
+                        }
+                    }
+                    num++;
+                }
+                symbs.Insert(0, '\"');
+                symbs.Add('\"');
+                symbs.Add(';');
+                Clipboard.SetText(new string(symbs.ToArray()));
+            }
 		}
 	}
 }
