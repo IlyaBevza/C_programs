49ed50f21d07a89ebc9d3d9ef176a6295a7edcfb
Ilya Bevza <ilya_bevza@hotmail.com>
2018-05-14 23:37:10 +0300

    ...

---
+++ C_Sharp/SaveRestoreSettings_1C/SaveRestoreSettings_1C/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("Сохранение и восстановление настроек 1C 8.0")]
+[assembly: AssemblyDescription ("Сохранение и восстановление пользовательских настроек 1С 8.0")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Бевза И.Н.  тел. +7(963) 637-45-51")]
+[assembly: AssemblyProduct ("SaveRestoreSettings_1C")]
+[assembly: AssemblyCopyright ("Бевза И.Н.   2018")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.*")]
---
+++ C_Sharp/fgup_tv/fgup_tv/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Дата: 16.04.2018
+ * Время: 4:35
+ */
+using System.Windows.Forms;
+using System;
+
+namespace fgup_tv
+{
+	
+	class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Clipboard.SetText("i.bevza@fguppromservis.ru");
+		}
+	}
+}
