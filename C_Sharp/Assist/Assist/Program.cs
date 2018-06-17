2a174141364b048cb92c9304d21b20fdf041f751
Ilya Bevza <ilya_bevza@hotmail.com>
2018-06-17 22:52:52 +0300

    Assist http request

---
+++ C_Sharp/Assist/Assist/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Дата: 17.06.2018
+ * Время: 17:43
+ */
+using System;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace Assist
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{ 
+			if(args.Length!=3) return;
+			string path=args[0];
+			string url=args[1];
+			string data=args[2];
+			string response=POST(url,data);
+			if(response==string.Empty) return;
+			using (FileStream file=new FileStream(path,FileMode.Create)){
+				byte[] bytedata=Encoding.UTF8.GetBytes(response);
+				file.Write(bytedata,0,bytedata.Length);
+				};
+			
+		}
+		
+
+
+private static string POST(string Url, string Data)
+{
+	ServicePointManager.SecurityProtocol=SecurityProtocolType.Tls11;
+	
+	WebRequest req = WebRequest.Create(Url);
+  req.Method = "POST";
+  req.Timeout = 100000;
+  req.ContentType = "application/x-www-form-urlencoded";
+  byte[] sentData = Encoding.UTF8.GetBytes(Data);
+  req.ContentLength = sentData.Length;
+  using(Stream sendStream = req.GetRequestStream()){
+  	sendStream.Write(sentData, 0, sentData.Length);
+  };
+  WebResponse response = req.GetResponse();
+  using (Stream stream = response.GetResponseStream())
+    {
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+}
+
+	}
+}
---
+++ C_Sharp/SaveRestore_1C_Settings/SaveRestore_1C_Settings/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace SaveRestore_1C_Settings
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			AppSettingsReader sreader=new AppSettingsReader();
+			string MainPosition=  (string)sreader.GetValue("MainPosition",typeof(string));
+			string BackupPosition=(string)sreader.GetValue("BackupPosition",typeof(string));
+			if(MainPosition==null || BackupPosition==null){
+				throw new Exception("Не найдены настройки программы");
+			}
+			DirectoryInfo mainDirectory=new DirectoryInfo(MainPosition);
+			
+			if(!mainDirectory.Exists){
+				throw new Exception("Не найдена папка настроек пользователей");
+			}
+			DirectoryInfo backupDirectory=new DirectoryInfo(BackupPosition);
+			if(!backupDirectory.Exists){
+				throw new Exception("Не найдена папка бэкапа настроек пользователей");
+			}
+			
+			//Копирование настроек
+			CopyFiles(mainDirectory,backupDirectory);
+			//Восстановление настроек
+			CopyFiles(backupDirectory,mainDirectory);
+
+		}
+		
+		// Перемещаются  только отсутствующие в месте назначения или большего размера файлы
+		static void CopyFiles(DirectoryInfo From,DirectoryInfo To){
+			FileInfo[] files=From.GetFiles("*.pfl");
+			if(files.Length==0){
+				throw new Exception("Не найдено ни одного файла pfl");
+			}
+			foreach(FileInfo file in files){
+				FileInfo substitutedFile=new FileInfo(To.FullName+"\\"+file.Name);
+				if(!substitutedFile.Exists || file.Length>substitutedFile.Length){
+					try{
+						file.CopyTo(substitutedFile.FullName,true);
+					}
+					catch{
+						continue;
+					}
+				}
+				                                      			
+			}
+		}
+	}
+}
